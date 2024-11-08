e86b29432dc2ed87f6b1a673f06bbe13592d58f6
carljohanj <carljohan.johansson@gmail.com>
2024-11-08 20:52:59 +0100

    Uppdatering

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * En rekursiv klass som t.ex kan användas för att bygga en länkad lista. Notera att Node innehåller en instans av sig
+ * själv. När vi skapar en ny länk i listan är det den här noden som vi länkar den samman med.
+ */
+public class Node
+{
+    int data;
+    Node node;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.node = null;
+    }
+}
