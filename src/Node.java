6fc1c1002e406098ea4bd0937b9ebaa5c81513a7
carljohanj <carljohan.johansson@gmail.com>
2024-12-15 18:07:58 +0100

    Changed Node to Generic

--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
+
 /**
  * En rekursiv klass som t.ex kan användas för att bygga en länkad lista.
  * Notera att Node innehåller en instans av sig själv. När vi skapar en ny
  * länk i listan är det den här noden som vi länkar den samman med.
  */
-public class Node
+public class Node<Type extends Number>
 {
-    int data;
-    Node node;      //En instans av Node inuti Node
+    Type data;
+    Node<Type> node;      //En instans av Node inuti Node
 
-    public Node(int data)
+    public Node(Type data)
     {
         this.data = data;
         this.node = null;
